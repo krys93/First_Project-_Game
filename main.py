4d16f9e266ec613c5f529a0a8caed74c1ce295e6
Christoher Obioha <110138213+krys93@users.noreply.github.com>
2022-08-09 12:06:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print ("Welcome to my first game!")
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+print("Hello", name, "you are", age,"years old.")
+
+
+
+''' ** = exponential, % mud operator = remainder after division, // interger division = no it divides, == is equal to, != not equal to  '''
